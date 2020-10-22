6c78dc4c8842623dabb471285c919cfe7d2e7944
Daniel <danielwebdeveloper220@gmail.com>
2020-10-22 19:14:11 -0400

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Chat from "./components/Chat";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
       <Router>
-        <Switch>
-          <Route path="/"></Route>
-        </Switch>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </div>
   );
